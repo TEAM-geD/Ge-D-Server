2478155148bb31c84206faca6edb8cf49c1fc3a3
nays111 <43735448+nays111@users.noreply.github.com>
2021-05-15 16:01:36 +0900

    feat : 직군 카테고리 전체 조회 API

--- src/main/java/com/example/ged/config/BaseResponseStatus.java
+++ src/main/java/com/example/ged/config/BaseResponseStatus.java
@@ -10,11 +10,12 @@ public enum BaseResponseStatus {
     // 1000 : 요청 성공
     SUCCESS(true, 1000, "요청에 성공하였습니다."),
 
-    //2000 : Request 오류
+    // 2000 : Request 오류
     EMPTY_JWT(false,2000,"JWT를 입력해주세요."),
     INVALID_JWT(false,2001,"유효하지 않은 JWT 입니다."),
 
-    //3000 : Response 오류
+    // 3000 : Response 오류
+    FAILED_TO_GET_USER_JOB_CATEGORIES(false,3001,"직군 카테고리 조회에 실패하였습니다."),
 
     // 4000 : Database 오류
     SERVER_ERROR(false, 4000, "서버와의 통신에 실패하였습니다."),
