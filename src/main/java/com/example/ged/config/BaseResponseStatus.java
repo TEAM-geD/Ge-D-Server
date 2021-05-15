dfafadd6bb8acf1cea06bf9d3f252a0db51869df
nays111 <43735448+nays111@users.noreply.github.com>
2021-05-15 13:14:15 +0900

    feat : userIdx 기준으로 JWT 발급

--- src/main/java/com/example/ged/config/BaseResponseStatus.java
+++ src/main/java/com/example/ged/config/BaseResponseStatus.java
@@ -10,6 +10,12 @@ public enum BaseResponseStatus {
     // 1000 : 요청 성공
     SUCCESS(true, 1000, "요청에 성공하였습니다."),
 
+    //2000 : Request 오류
+    EMPTY_JWT(false,2000,"JWT를 입력해주세요."),
+    INVALID_JWT(false,2001,"유효하지 않은 JWT 입니다."),
+
+    //3000 : Response 오류
+
     // 4000 : Database 오류
     SERVER_ERROR(false, 4000, "서버와의 통신에 실패하였습니다."),
     DATABASE_ERROR(false, 4001, "데이터베이스 연결에 실패하였습니다.");
