3cb967f6bfc76f8ceaedff585f089e266541ad02
nays111 <43735448+nays111@users.noreply.github.com>
2021-05-15 16:34:36 +0900

    feat : UserInfo controller,repository 생성

---
+++ src/main/java/com/example/ged/src/user/UserInfoController.java
@@ -0,0 +1,12 @@
+package com.example.ged.src.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
+public class UserInfoController {
+
+}
