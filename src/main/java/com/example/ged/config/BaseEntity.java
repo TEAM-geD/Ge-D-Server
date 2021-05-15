dd074da8e085018fb235b0f5b0f2422c01229ea3
nays111 <43735448+nays111@users.noreply.github.com>
2021-05-15 12:40:17 +0900

    feat : BaseEntity 생성

---
+++ src/main/java/com/example/ged/config/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.example.ged.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @CreationTimestamp
+    @Column(name = "createdAt", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updatedAt", nullable = false)
+    private Date updatedAt;
+}
