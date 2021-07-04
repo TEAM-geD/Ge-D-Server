63b4e82e687b16eac23b7073fc3fea07990e7945
nays111 <43735448+nays111@users.noreply.github.com>
2021-07-04 18:01:37 +0900

    feat : 프로젝트 참여 멤버 리스트 조회 API

--- src/main/java/com/example/ged/src/projectApply/ProjectApplyController.java
+++ src/main/java/com/example/ged/src/projectApply/ProjectApplyController.java
@@ -2,6 +2,7 @@ package com.example.ged.src.projectApply;
 
 import com.example.ged.config.BaseException;
 import com.example.ged.config.BaseResponse;
+import com.example.ged.src.projectApply.models.dto.GetProjectMembersRes;
 import com.example.ged.src.projectApply.models.dto.PostProjectApplyReq;
 import com.example.ged.utils.JwtService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,6 +18,7 @@ public class ProjectApplyController {
 
     private final JwtService jwtService;
     private final ProjectApplyService projectApplyService;
+    private final ProjectApplyProvider projectApplyProvider;
 
     /**
      * 2021-07-04
@@ -37,9 +39,15 @@ public class ProjectApplyController {
         }
     }
 
-    @PostMapping("projects/dis-join")
+    /**
+     * 2021-07-04
+     * 프로젝트 참여 신청 취소하기 API
+     * @param postProjectApplyReq
+     * @return
+     */
+    @PostMapping("/projects/dis-join")
     @ResponseBody
-    @Operation(summary = "프로젝트 참여 신청 취소하기")
+    @Operation(summary = "프로젝트 참여 신청 취소하기 API")
     public BaseResponse<String> deleteProjectApply(@RequestBody PostProjectApplyReq postProjectApplyReq){
         try{
             Integer userIdx = jwtService.getUserIdx();
@@ -49,4 +57,24 @@ public class ProjectApplyController {
             return new BaseResponse<>(exception.getStatus());
         }
     }
+
+    /**
+     * 2021-07-04
+     * 프로젝트 참여 멤버 리스트 조회 API
+     * @param projectIdx
+     * @return
+     */
+    @GetMapping("/projects/{projectIdx}/members")
+    @ResponseBody
+    @Operation(summary = "프로젝트 참여 멤버 리스트 조회 API")
+    public BaseResponse<GetProjectMembersRes> getProjectMember(@PathVariable Integer projectIdx){
+        try{
+            Integer userIdx = jwtService.getUserIdx();
+            GetProjectMembersRes getProjectMembersRes = projectApplyProvider.getProjectMembers(projectIdx);
+            return new BaseResponse<>(SUCCESS ,  getProjectMembersRes);
+        }catch(BaseException exception){
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
+
 }
--- src/main/java/com/example/ged/src/projectApply/ProjectApplyRepository.java
+++ src/main/java/com/example/ged/src/projectApply/ProjectApplyRepository.java
@@ -5,6 +5,11 @@ import com.example.ged.src.projectApply.models.ProjectApply;
 import com.example.ged.src.user.models.UserInfo;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProjectApplyRepository extends CrudRepository<ProjectApply,Integer> {
     ProjectApply findAllByProjectAndUserInfoAndStatus(Project project, UserInfo userInfo,String status);
+
+
+    List<ProjectApply> findAllByProjectAndApplyStatusAndStatus(Project project, String applyStatus, String status);
 }
